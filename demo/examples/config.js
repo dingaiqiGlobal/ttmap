3180a108d28c31c07e027e5216b614af1d2106be
ding <1621029739@qq.com>
2022-04-28 16:32:02 +0800

    增加了一些示例

--- demo/examples/config.js
+++ demo/examples/config.js
@@ -12,107 +12,221 @@ var exampleConfig = {
             "地图": {
                 name: "地图",
                 content: [
-                {
-                    name: "百度地图",
-                    thumbnail: "xingkong.gif",
-                    fileName: "baidu"
-                },{
-                    name: "bing地图",
-                    thumbnail: "xingkong.gif",
-                    fileName: "bing"
-                },{
-                    name: "cesium地图",
-                    thumbnail: "xingkong.gif",
-                    fileName: "cesium"
-                },{
-                    name: "高德地图",
-                    thumbnail: "xingkong.gif",
-                    fileName: "gaode"
-                },{
-                    name: "mapbox地图",
-                    thumbnail: "xingkong.gif",
-                    fileName: "mapbox"
-                },{
-                    name: "天地图地图",
-                    thumbnail: "xingkong.gif",
-                    fileName: "tdt"
-                },{
-                    name: "wms地图",
-                    thumbnail: "xingkong.gif",
-                    fileName: "wms"
-                },{
-                    name: "wmts地图",
-                    thumbnail: "xingkong.gif",
-                    fileName: "wmts"
-                },{
-                    name: "xyz地图",
-                    thumbnail: "xingkong.gif",
-                    fileName: "xyz"
-                }]},
+                    {
+                        name: "百度地图",
+                        thumbnail: "xingkong.gif",
+                        fileName: "baidu"
+                    }, {
+                        name: "bing地图",
+                        thumbnail: "xingkong.gif",
+                        fileName: "bing"
+                    }, {
+                        name: "cesium地图",
+                        thumbnail: "xingkong.gif",
+                        fileName: "cesium"
+                    }, {
+                        name: "高德地图",
+                        thumbnail: "xingkong.gif",
+                        fileName: "gaode"
+                    }, {
+                        name: "mapbox地图",
+                        thumbnail: "xingkong.gif",
+                        fileName: "mapbox"
+                    }, {
+                        name: "天地图地图",
+                        thumbnail: "xingkong.gif",
+                        fileName: "tdt"
+                    }, {
+                        name: "wms地图",
+                        thumbnail: "xingkong.gif",
+                        fileName: "wms"
+                    }, {
+                        name: "wmts地图",
+                        thumbnail: "xingkong.gif",
+                        fileName: "wmts"
+                    }, {
+                        name: "xyz地图",
+                        thumbnail: "xingkong.gif",
+                        fileName: "xyz"
+                    }]
+            },
             "矢量": {
                 name: "矢量",
                 content: [{
                     name: "wfs",
                     thumbnail: "xingkong.gif",
                     fileName: "wfs"
-                },{
+                }, {
                     name: "geojson",
                     thumbnail: "xingkong.gif",
                     fileName: "geojson"
-                }]},
+                }]
+            },
             "三维场景": {
                 name: "三维场景",
                 content: [{
                     name: "天空盒",
                     thumbnail: "xingkong.gif",
                     fileName: "skybox"
-                },{
+                }, {
                     name: "太阳月亮",
                     thumbnail: "xingkong.gif",
                     fileName: "sunmoon"
-                },{
+                }, {
                     name: "大气",
                     thumbnail: "xingkong.gif",
                     fileName: "skyAtmosphere"
-                }]}
-    }},
+                }]
+            }
+        }
+    },
     "2D": {
         name: "2D",
         content: {
-            "地球": {
-                name: "地球",
+            "地图示例": {
+                name: "地图示例",
                 content: [{
-                    name: "星空背景",
+                    name: "OMS地图",
                     thumbnail: "xingkong.gif",
-                    fileName: "es6_t1"
-                },{
-                    name: "星空背景2",
+                    fileName: "ol_Map_OMS"
+                }, {
+                    name: "bing地图",
                     thumbnail: "xingkong.gif",
-                    fileName: "es6_t1"
-                }]},
-            "地球2": {
-                name: "地球",
+                    fileName: "ol_Map_Bing"
+                }, {
+                    name: "谷歌地图-",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Map_GoogleMap"
+                }, {
+                    name: "高德地图",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Map_GaoDeMap"
+                }, {
+                    name: "天地图地图-",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Map_TiandituMap"
+                }, {
+                    name: "百度地图-",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Map_BaiduMap"
+                }, {
+                    name: "ArcGIS地图",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Map_ArcGIS"
+                },
+                {
+                    name: "矢量切片MapBox",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Map_MVT"
+                },
+                ]
+            },
+            "控件": {
+                name: "控件",
                 content: [{
-                    name: "星空背景",
+                    name: "设置背景",
                     thumbnail: "xingkong.gif",
-                    fileName: "es6_t1"
-                },{
-                    name: "星空背景2",
+                    fileName: "ol_Control_Background"
+                }, {
+                    name: "导航控件",
                     thumbnail: "xingkong.gif",
-                    fileName: "es6_t1"
-                }]},
-            "地球3": {
-                name: "地球",
+                    fileName: "ol_Control_ZoomSlider"
+                }, {
+                    name: "鼠标位置",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Control_MousePosition"
+                }, {
+                    name: "比例尺",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Control_ScaleLine"
+                }, {
+                    name: "全屏",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Control_FullScreen"
+                }, {
+                    name: "鹰眼图",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Control_OverviewMap"
+                }, {
+                    name: "测量",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Control_Measure"
+                }, {
+                    name: "控件自定义样式",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Control_CustomStyles"
+                }, {
+                    name: "视图基础操作",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Control_ViewBasic"
+                }, {
+                    name: "视图动画",
+                    thumbnail: "xingkong.gif",
+                    fileName: "ol_Control_ViewAnimation"
+                },
+
+                ]
+            },
+            "OGC": {
+                name: "OGC",
                 content: [{
-                    name: "星空背景",
+                    name: "WFS",
                     thumbnail: "xingkong.gif",
                     fileName: "es6_t1"
-                },{
-                    name: "星空背景2",
+                }, {
+                    name: "WMS",
                     thumbnail: "xingkong.gif",
                     fileName: "es6_t1"
-                }]}
-        }}
+                },
+                {
+                    name: "WMTS",
+                    thumbnail: "xingkong.gif",
+                    fileName: "es6_t1"
+                }, {
+                    name: "TMS",
+                    thumbnail: "xingkong.gif",
+                    fileName: "es6_t1"
+                },
+                ]
+            },
+            "图形绘制": {
+                name: "图形绘制",
+                content: [{
+                    name: "test1",
+                    thumbnail: "xingkong.gif",
+                    fileName: "es6_t1"
+                }, {
+                    name: "test2",
+                    thumbnail: "xingkong.gif",
+                    fileName: "es6_t1"
+                }]
+            },
+            "地图标注": {
+                name: "地图标注",
+                content: [{
+                    name: "test1",
+                    thumbnail: "xingkong.gif",
+                    fileName: "es6_t1"
+                }, {
+                    name: "test2",
+                    thumbnail: "xingkong.gif",
+                    fileName: "es6_t1"
+                }]
+            },
+            "其他功能": {
+                name: "其他功能",
+                content: [{
+                    name: "test1",
+                    thumbnail: "xingkong.gif",
+                    fileName: "es6_t1"
+                }, {
+                    name: "test2",
+                    thumbnail: "xingkong.gif",
+                    fileName: "es6_t1"
+                }]
+            }
+        }
+    }
 };
 /**
  *key值：为exampleConfig配置的key值或者fileName值

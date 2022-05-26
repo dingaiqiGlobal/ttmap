9e20d9255bcfe34a7cf2ad632344dc68a1067d0f
home_ding <1621029739@qq.com>
2022-05-26 22:44:08 +0800

    增加事件栏

--- demo/examples/config.js
+++ demo/examples/config.js
@@ -6,80 +6,6 @@ var identification = {
 };
 
 var exampleConfig = {
-    "3D": {
-        name: "3D",
-        content: {
-            "地图": {
-                name: "地图",
-                content: [
-                    {
-                        name: "百度地图",
-                        thumbnail: "xingkong.gif",
-                        fileName: "baidu"
-                    }, {
-                        name: "bing地图",
-                        thumbnail: "xingkong.gif",
-                        fileName: "bing"
-                    }, {
-                        name: "cesium地图",
-                        thumbnail: "xingkong.gif",
-                        fileName: "cesium"
-                    }, {
-                        name: "高德地图",
-                        thumbnail: "xingkong.gif",
-                        fileName: "gaode"
-                    }, {
-                        name: "mapbox地图",
-                        thumbnail: "xingkong.gif",
-                        fileName: "mapbox"
-                    }, {
-                        name: "天地图地图",
-                        thumbnail: "xingkong.gif",
-                        fileName: "tdt"
-                    }, {
-                        name: "wms地图",
-                        thumbnail: "xingkong.gif",
-                        fileName: "wms"
-                    }, {
-                        name: "wmts地图",
-                        thumbnail: "xingkong.gif",
-                        fileName: "wmts"
-                    }, {
-                        name: "xyz地图",
-                        thumbnail: "xingkong.gif",
-                        fileName: "xyz"
-                    }]
-            },
-            "矢量": {
-                name: "矢量",
-                content: [{
-                    name: "wfs",
-                    thumbnail: "xingkong.gif",
-                    fileName: "wfs"
-                }, {
-                    name: "geojson",
-                    thumbnail: "xingkong.gif",
-                    fileName: "geojson"
-                }]
-            },
-            "三维场景": {
-                name: "三维场景",
-                content: [{
-                    name: "天空盒",
-                    thumbnail: "xingkong.gif",
-                    fileName: "skybox"
-                }, {
-                    name: "太阳月亮",
-                    thumbnail: "xingkong.gif",
-                    fileName: "sunmoon"
-                }, {
-                    name: "大气",
-                    thumbnail: "xingkong.gif",
-                    fileName: "skyAtmosphere"
-                }]
-            }
-        }
-    },
     "2D": {
         name: "2D",
         content: {
@@ -91,37 +17,37 @@ var exampleConfig = {
                     fileName: "ol_Map_OMS"
                 }, {
                     name: "bing地图",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Map_Bing"
                 }, {
                     name: "谷歌地图-封闭",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Map_GoogleMap"
                 }, {
                     name: "高德地图",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Map_GaoDeMap"
                 }, {
                     name: "天地图地图",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Map_TiandituMap"
                 }, {
                     name: "百度地图-封闭",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Map_BaiduMap"
                 }, {
                     name: "ArcGIS地图",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Map_ArcGIS"
                 },
                 {
                     name: "矢量切片地图MapBox-",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Map_MVT"
                 },
                 {
                     name: "瓦片网格描述",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Map_CanvasTiles"
                 },
                 ]
@@ -130,43 +56,43 @@ var exampleConfig = {
                 name: "控件",
                 content: [{
                     name: "设置背景",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Control_Background"
                 }, {
                     name: "导航控件",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Control_ZoomSlider"
                 }, {
                     name: "鼠标位置",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Control_MousePosition"
                 }, {
                     name: "比例尺",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Control_ScaleLine"
                 }, {
                     name: "全屏",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Control_FullScreen"
                 }, {
                     name: "鹰眼图",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Control_OverviewMap"
                 }, {
                     name: "测量",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Control_Measure"
                 }, {
                     name: "控件自定义样式",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Control_CustomStyles"
                 }, {
                     name: "视图基础操作",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Control_ViewBasic"
                 }, {
                     name: "视图动画",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Control_ViewAnimation"
                 },
 
@@ -176,40 +102,40 @@ var exampleConfig = {
                 name: "OGC",
                 content: [{
                     name: "WFS",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_OMS_WFS"
                 }, {
                     name: "WMS",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_OMS_WMS"
                 },
                 {
                     name: "WMTS",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_OMS_WMTS"
                 }, {
                     name: "TMS",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_OMS_TMS"
                 }, {
                     name: "WFS_GetFeature",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_OMS_WFS_GetFeature"
                 }, {
                     name: "WFS_Filter",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_OMS_WFS_Filter"
                 }, {
                     name: "WFS_CQL_Filter",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_OMS_WFS_CQL_Filter"
                 },{
                     name: "WFS_Transaction",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_OMS_TMS"
                 }, {
                     name: "WMS_GetFeatureInfo",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_OMS_WMS_GetFeatureInfo"
                 },
                 ]
@@ -218,19 +144,19 @@ var exampleConfig = {
                 name: "图层样式",
                 content: [{
                     name: "唯一值渲染",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Layer_UniqueValue"
                 }, {
                     name: "断点值渲染",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Layer_BreakValue"
                 }, {
                     name: "热力图渲染",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Layer_HeatMap"
                 }, {
                     name: "聚散渲染",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Layer_Cluster"
                 }
                 ]
@@ -239,15 +165,15 @@ var exampleConfig = {
                 name: "几何形状",
                 content: [{
                     name: "绘制几何图形",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Geometry_DrawFeatures"
                 }, {
                     name: "样式编辑",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Geometry_FeaturesStyle"
                 }, {
                     name: "图形交互编辑",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Geometry_ModifyFeatures"
                 }
             ]
@@ -255,24 +181,32 @@ var exampleConfig = {
             "地图事件": {
                 name: "地图事件",
                 content: [{
+                    name: "概述",
+                    thumbnail: "",
+                    fileName: "ol_Event_Base"
+                },{
                     name: "WFS单击事件(getFeaturesAtPixel)",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Event_WFS_Singleclick_getFeaturesAtPixel"
+                },{
+                    name: "WFS单击事件(forEachFeatureAtPixel)",
+                    thumbnail: "",
+                    fileName: "ol_Event_WFS_Singleclick_forEachFeatureAtPixel"
                 }, {
                     name: "WFS单击事件(select)",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Event_WFS_Singleclick_Select"
                 },{
                     name: "WFS移入移除事件（forEachFeatureAtPixel）",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Event_WFS_Pointmove_forEachFeatureAtPixel"
                 },{
                     name: "WFS移入移除事件（select）",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Event_WFS_Pointmove_Select"
                 },{
                     name: "WMS移入移除事件（hit）",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "ol_Event_WMS_Pointmove_hit"
                 },
             ]
@@ -280,24 +214,24 @@ var exampleConfig = {
             "地图标注": {
                 name: "地图标注",
                 content: [{
-                    name: "test1",
-                    thumbnail: "xingkong.gif",
-                    fileName: "es6_t1"
+                    name: "气泡框",
+                    thumbnail: "",
+                    fileName: "ol_Marker_Popup"
                 }, {
                     name: "test2",
-                    thumbnail: "xingkong.gif",
-                    fileName: "es6_t1"
+                    thumbnail: "",
+                    fileName: "ol_Marker_Popup"
                 }]
             },
             "其他功能": {
                 name: "其他功能",
                 content: [{
                     name: "test1",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "es6_t1"
                 }, {
                     name: "test2",
-                    thumbnail: "xingkong.gif",
+                    thumbnail: "",
                     fileName: "es6_t1"
                 }]
             }
